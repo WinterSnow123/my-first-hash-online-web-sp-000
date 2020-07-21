8b690796a3163a66c2d04d64d797cbf4d342f38f
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-21 18:15:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,10 @@
 def my_hash
-  my_hash = {"first_child" => "Canyon", "second_child" => "Mesa", "third_child" => "Callum"}
+  my_hash = {"first child" => "Canyon", "second child" => "Mesa", "third child" => "Callum"}
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information:
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
-
+  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
 end
 
 def retrieval
